677d1b4a729da87e12cf2d1d5b781285b48a75cb
smeb y <48400087+a3165458@users.noreply.github.com>
2024-05-11 14:15:46 +0800

    Update airchains.sh

--- airchains.sh
+++ airchains.sh
@@ -178,7 +178,7 @@ EOF
 
 # 给自己地址验证者质押
 function delegate_self_validator() {
-read -p "请输入质押代币数量: " math
+read -p "请输入质押代币数量,比如你有1个amf,请输入1000000，以此类推: " math
 read -p "请输入钱包名称: " wallet_name
 junctiond tx staking delegate $(junctiond keys show $wallet_name --bech val -a)  ${math}amf --from $wallet_name --chain-id=junction --fees 5000amf --node $junctiond_RPC_PORT  -y
 
